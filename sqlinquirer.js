af7c4fe3ec11a1da3523b03d781e96bdb7497c73
JayRodButray <jayrodbutray@gmail.com>
2023-07-29 13:07:30 -0700

    added first functions

--- sqlinquirer.js
+++ sqlinquirer.js
@@ -7,13 +7,22 @@ const db = mysql.createConnection();
 function getAllEmployees() {
     return db.query('select * from employees');
 }
-
+function getAllDepartments(){
+    return db.query('select * from departments');
+}
 inquirer.prompt {[
     {
-        name: '',
+        name: 'action',
         type: 'list',
         list: ['action1', 'action2'],
     }
-]}.then((answers) => {
-
+]}.then(async (answers) => {
+    switch (answers.action) {
+        case 'action1':
+            const employees = await getAllEmployees();
+            //displays employees
+            break;
+        case 'action2':
+            const departments = await getAllDepartments();
+    }
 })
